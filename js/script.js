a8dbfbf65f0c42a764aad20d0faf24fef7eb79d5
박소예 <soye@bagsoyeui-MacBookAir.local>
2023-09-12 19:26:00 +0900

    날짜 클릭시 왼쪽 달력표시 연동 및 todoList 등록 및 삭제

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ const Today_date = document.getElementById("resultDate");
 const Today_day = document.getElementById("resultDay");
 let Today_year = document.querySelector(".year");
 let Today_month = document.querySelector(".monthBox");
-const month_Cont = document.querySelector(".calendar_Content li");
+let month_Cont = document.querySelector(".calendar_Content li");
 const calendar_days = document.querySelector(".calendar_Date");
 
 //윤월에 따른 달력페이지 설정
@@ -35,12 +35,11 @@ if(first.getFullYear() % 4 === 0){
 
 console.log(pageYear[first.getMonth()]);
 
-
-
-    Today_date.innerHTML = `<p>${dates} day</p>`;
-    Today_day.innerHTML = `<p>${Days_array[days]}</p>`;
     Today_month.innerHTML = Month_array[first.getMonth()];
     Today_year.innerHTML = first.getFullYear();
+
+
+
 
 // Calendar 표시
 let calendarBody = document.querySelector(".calendar_Content");
@@ -101,11 +100,25 @@ function del_Canlendar(){
 
 const prevBtn  = document.querySelector(".prev");
 const nextBtn  = document.querySelector(".next");
-const inputBox = document.getElementById("inputBox");
 
-prevBtn.addEventListener("click" , () => {
+function prev(){
 
     inputBox.value = "";
+    const $lists = document.querySelectorAll('.todoContent > li');
+
+    $lists.forEach(function(e){
+      
+        e.remove();
+
+    });
+
+    const $btns = document.querySelectorAll('.todoContent li > span');
+
+    $btns.forEach(function(e1){
+      
+        e1.remove();
+
+    });
     
     if(pageFrist.getMonth() === 1){
  
@@ -135,11 +148,33 @@ prevBtn.addEventListener("click" , () => {
     Today_year.innerHTML = first.getFullYear();
     showCalendar();
     del_Canlendar();
-});
+    leftCalendar();
 
-nextBtn.addEventListener("click" , () => {
+    clickDate1 = document.getElementById(today.getDate());
+    clickDate1.classList.add("active");
+    clickStart();
+    resetTodo();
+};
+
+function next(){
 
     inputBox.value = "";
+
+  const $lists = document.querySelectorAll('.todoContent > li');
+
+    $lists.forEach(function(e){
+      
+        e.remove();
+
+    });
+
+    const $btns = document.querySelectorAll('.todoContent li > span');
+
+    $btns.forEach(function(e1){
+      
+        e1.remove();
+
+    });
     
     if(pageFrist.getMonth() === 12){
  
@@ -152,6 +187,7 @@ nextBtn.addEventListener("click" , () => {
          if(first.getFullYear() % 4 === 0){
 
             pageYear = leapYear;
+
          }else{
 
             pageYear = leapNotyear;
@@ -168,7 +204,204 @@ nextBtn.addEventListener("click" , () => {
     Today_year.innerHTML = first.getFullYear();
     showCalendar();
     del_Canlendar();
-})
+    leftCalendar();
+
+    clickDate1 = document.getElementById(today.getDate());
+    clickDate1.classList.add("active");
+    clickStart();
+    resetTodo();
+};
+
+
+//날짜 클릭시 왼쪽 화면 변경
+
+var clickDate1 = document.getElementById([today.getDate()]);
+clickDate1.classList.add('active');
+
+function leftCalendar(){
+
+    Today_day.innerHTML =  Days_array[today.getDay()];
+    Today_date.innerHTML = today.getDate();
+
+}
+
+leftCalendar();
+
+prevBtn.addEventListener("click" , prev );
+nextBtn.addEventListener("click" , next );
+
+let dayGroup = [];
+
+function clickStart(){
+
+    for(let i = 1; i <= pageYear[first.getMonth()]; i++){
+
+        dayGroup[i] = document.getElementById(i);
+        dayGroup[i].addEventListener("click" , changeToday);
+    }
+
+}
+
+clickStart();
+
+function changeToday(e){
+
+    for(let i = 1; i <= pageYear[first.getMonth()]; i++){
+
+        if(dayGroup[i].classList.contains('active')){
+
+            dayGroup[i].classList.remove('active');
+            console.log("active 삭제");
+        }
+    }
+
+    clickDate1 = e.currentTarget;
+    console.log(clickDate1);
+    clickDate1.classList.add("active");
+    today = new Date(today.getFullYear(), today.getMonth(), clickDate1.id);
+    leftCalendar();
+    keyValue = today.getFullYear() + "" + today.getMonth() + "" + today.getDate();
+
+    console.log("작동중");
+    resetTodo();
+
+};
+
+
+//todoList 목록 업데이트 및 삭제
+
+let inputBox = document.getElementById('inputBox');
+let inputDate = document.getElementById('inputData');
+let inputList = document.querySelector('.todoContent');
+
+const delBtn = 'X';
+
+inputDate.addEventListener("click" , addTodoList);
+
+let dateCnt = 1;
+let keyValue = today.getFullYear() + '' + today.getMonth() + '' + today.getDate();
+
+
+
+
+function resetTodo(){
+
+    keyValue = today.getFullYear() + '' + today.getMonth()+ '' + today.getDate();
+
+    if(todoList[keyValue] === undefined){
+
+        inputList.innerHTML = '';
+        todoList[keyValue] = [];
+        const $lists = document.querySelectorAll('.todoContent > li');
+        $lists.forEach(function(e){
+
+          e.remove();
+
+        });
+
+        const $btns = document.querySelectorAll('.todoContent li > span');
+
+        $btns.forEach(function(e1){
+          e1.remove();
+        });
+
+    }else if(todoList[keyValue].length === 0){
+
+        inputList.innerHTML = '';
+        const $lists = document.querySelectorAll('.todoContent > li');
+        $lists.forEach(function(e){
+          e.remove();
+        });
+        const $btns = document.querySelectorAll('.todoContent li > span');
+        $btns.forEach(function(e1){
+          e1.remove();
+        });
+
+    }else{
+         const $lists = document.querySelectorAll('.todoContent > li');
+            $lists.forEach(function(e){
+              e.remove();
+            });
+            const $btns = document.querySelectorAll('.todoContent li > span');
+            $btns.forEach(function(e1){
+              e1.remove();
+            });
+
+            var $list = document.createElement('div');
+
+            for(var i = 0; i < todoList[keyValue].length; i++){
+
+                var $list = document.createElement('li');
+                $list.textContent = '-' + todoList[keyValue][i];
+                var $btn = document.createElement('span');
+                $btn.setAttribute('id', dateCnt+keyValue);
+                $btn.setAttribute('class', 'delete');
+                $btn.textContent = delBtn;
+                inputList.appendChild($list);
+                $list.appendChild($btn);
+
+                $list.addEventListener('click', checkList);
+                $btn.addEventListener('click', deleteTodo);
+
+                inputBox.value = '';
+
+                function deleteTodo(){
+
+                    $list.remove();
+                    $btn.remove();
+                    console.log($list);
+                    console.log($btn);
+        
+
+                }
+            }
+        }
+    
+    }
+
+
+let todoList = [];
+todoList[keyValue] = [];
+
+function addTodoList(){
+
+    let $list = document.createElement('li');
+    $list.textContent = inputBox.value;
+
+    let $btn = document.createElement('span');
+    $btn.setAttribute('class' , 'delete');
+    $btn.setAttribute('id' , dateCnt+keyValue);
+    $btn.textContent = delBtn;
+
+    inputList.appendChild($list);
+    $list.appendChild($btn);
+
+    todoList[keyValue].push(inputBox.value);
+    dateCnt++;
+    inputBox.value = '';
+    $list.addEventListener("click" , checkList);
+    $btn.addEventListener("click" , deleteTodo);
+
+    function deleteTodo(){
+
+        $list.remove();
+        $btn.remove();
+    }
+
+}
+
+console.log(keyValue);
+
+function checkList(e){
+
+    e.currentTarget.classList.add('checked');
+}
+
+
+
+
+
+
 
 
 
